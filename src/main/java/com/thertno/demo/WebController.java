786b0e6ee55ef6b1aaf4d01d8adeb57f54e21c96
e <thertno@aliyun.com>
2017-12-25 17:45:18 +0800

    issul-1 commit

---
+++ src/main/java/com/thertno/demo/WebController.java
@@ -0,0 +1,13 @@
+package com.thertno.demo;
+
+/**
+ * Description:
+ *
+ * @author: pengl
+ * @create: 2017-12-25 17:44
+ */
+public class WebController {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
